bfb2c3f478fd89db0459da94b98b836af9855766
Tydik42 <tgaraev653@gmail.com>
2024-02-25 20:56:11 +0300

    hot fix

--- include/Ipv4.h
+++ include/Ipv4.h
@@ -1,20 +1,22 @@
 #pragma once
 #include <string>
-#include <oastream>
+#include <ostream>
 #include <vector>
 #include <sstream>
+#include <cstdint>
+#include <array>
 
 class Ipv4
 {
 public:
-    Ipv4() : arr_ip{0, 0, 0, 0}
+    Ipv4() : arr_ip{0, 0, 0, 0} {};
 
-    explicit Ipv4(const std::string& Ipv4, const char delimiter = '.');
+    explicit Ipv4(const std::string& Ipv4, char delimiter = '.');
 
-    bool contains(const uint16_1 value);
+    bool contains(int value);
 
     bool operator > (const Ipv4& other);
 
 private:
-    std::array<uint16_t, 4> arr_ip;
+    std::array<uint16_t , 4> arr_ip;
 };
