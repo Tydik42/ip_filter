eac52ecd6802c68450adf4fd92d4dba3c76b565f
Tydik42 <tgaraev653@gmail.com>
2024-02-28 21:52:34 +0300

    utils fix

--- include/utils.h
+++ include/utils.h
@@ -7,24 +7,9 @@
 #include <vector>
 
 template<typename C, typename P>
-void print(C container, P predicate)
-{
-    typedef typename C::value_type element_type;
-    std::copy_if(container.begin(), container.end(), std::ostream_iterator<element_type>(std::cout, "\n"),
-                 predicate);
-}
+void print(C container, P predicate);
 
-std::vector<Ipv4> read_ip()
-{
-    std::vector<Ipv4> data;
-    std::string input;
-    while (std::getline(std::cin, input))
-    {
-        auto address = Ipv4(input.substr(0, input.find('\t')));
-        data.push_back(address);
-    }
-    return data;
-}
+std::vector<Ipv4> read_ip();
 
 auto no_filtering = []() { return true; };
 
