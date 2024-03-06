661b669139815400a0b65ea3d077850493498afc
Tydik42 <tgaraev653@gmail.com>
2024-03-06 12:02:16 +0300

    print fix

--- include/utils.h
+++ include/utils.h
@@ -7,23 +7,6 @@
 #include <vector>
 #include <regex>
 
-template<typename C, typename P>
-void print(C container, P predicate);
-
 std::vector<Ipv4> read_ip();
-
-auto no_filtering = []() { return true; };
-
-// Print 1.*.*.*
-auto starts_from_1 = [](const Ipv4 ip) { return ip.first() == 1; };
-
-// Print 46.70.*.*
-auto starts_from_4670 = [](const Ipv4 ip) { return ip.first() == 46 && ip.second() == 70; };
-
-// Print 46.*.*.* || *.46.*.* || *.*.46.* || *.*.*.46
-auto has_any_46 = [](const Ipv4 ip) {
-    return ip.first() == 46 || ip.second() == 46 || ip.third() == 46 ||
-           ip.fourth() == 46;
-};
 
 bool is_valid_ip(const std::string &ip);
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,13 +1,5 @@
 #include "utils.h"
 
-template<typename C, typename P>
-void print(C container, P predicate)
-{
-    typedef typename C::value_type element_type;
-    std::copy_if(container.begin(), container.end(), std::ostream_iterator<element_type>(std::cout, "\n"),
-                 predicate);
-}
-
 std::vector<Ipv4> read_ip()
 {
     std::vector<Ipv4> data;
